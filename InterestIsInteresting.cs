bdef7424626e5199c04a007d88da22875c0848c4
mjohnson <mr.markoeddie@gmail.com>
2025-07-15 20:47:30 -0400

    First commit

--- InterestIsInteresting.cs
+++ InterestIsInteresting.cs
@@ -21,37 +21,21 @@ static class SavingsAccount
         }
     }
 
-    public static decimal Interest(decimal balance)
-    {
-        //decimal b = balance;
-        //float f = InterestRate(balance);
-        //decimal result = b * ((decimal)f);
-        //return result;
+    public static decimal Interest(decimal balance) => balance * (decimal)InterestRate(balance) * 0.01m;
 
-        //decimal b = balance;
-        //float f = InterestRate(balance);
-        //float result = ((float)b) * f; 
-
-        //return result;
-       return balance * (decimal)InterestRate(balance) * 0.01m;
-        
-    }
-
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        return Interest(balance) + balance;
-    }
-
+    public static decimal AnnualBalanceUpdate(decimal balance) => Interest(balance) + balance;
+    
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        //decimal result = (targetBalance - balance) / Interest(balance);
-        //return (int)result;
-        return ((int)targetBalance - (int)balance) / (int)Interest(balance);
+        int iterationCount = 0;
 
-        //do
-        //{
-        //    ((int)targetBalance - (int)balance) / (int)Interest(balance);
-        //}
-        //while (balance < targetBalance) ;
+        do
+        {
+            balance = balance + Interest(balance);
+            iterationCount++;
+        }
+        while (balance < targetBalance);
+
+        return iterationCount;
     }
 }
